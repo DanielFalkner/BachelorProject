0cd969f2f8bd6dc8cc6ffcb54cf8d33f36a182bb
DanielFalkner <da.falkner@gmx.at>
2022-07-13 11:16:56 +0200

    Test Class added

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Test {
+
+    int x = 10;
+
+    public int getSum(){
+        int sum = 0;
+        for(int i = 0; i < x; i++){
+            sum = i;
+        }
+        return sum;
+    }
+}
